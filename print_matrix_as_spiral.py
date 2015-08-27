2cd56b895720deba4c37b9b37aea14ce8cd70f29
schottkey <schottkey@gmail.com>
2015-08-27 20:50:55 +0100

    Restructured & added a script

---
+++ print_matrix_as_spiral.py
@@ -0,0 +1,20 @@
+"""Print a n x m matrix as a spiral."""
+p, l = 0, 5
+flag = False
+
+
+def spiral(p, l, flag):
+    """Docstring."""
+    m, n = 50, 50
+    matrix = [list(range(m)) for _ in range(n)]
+    for row in matrix:
+        for n in row:
+            print('%s%d' % (' '*p, n))
+            if p == l or p == 0:
+                flag = not flag
+            if flag:
+                p += 1
+            else:
+                p -= 1
+
+spiral(p, l, flag)
