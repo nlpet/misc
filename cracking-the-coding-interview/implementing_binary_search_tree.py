d2db9927ede9f6671b1bbfcd7c8139c7495429de
schottkey <schottkey@gmail.com>
2015-08-30 10:46:56 +0100

    Reformatted the file - tab of 4 spaces and added docstrings

--- cracking-the-coding-interview/implementing_binary_search_tree.py
+++ cracking-the-coding-interview/implementing_binary_search_tree.py
@@ -1,87 +1,69 @@
-
-'''
-Implement a binary search tree
+r"""
+Implement a binary search tree.
 
      9
     / \
-   5   3 
+   5   3
   / \ / \
  1  3 4  6
- 
-'''
+
+"""
 
 
 class BinarySearchTree:
-  def __init__(self):
-    self.root = None
-    self.size = 0
-    
-  def length(self):
-    return self.size
-    
-  def __len__(self)
-    return self.size
-    
-  def __iter__(self):
-    return self.root.__iter__()
-    
-  
-    
+
+    """Binary search tree."""
+
+    def __init__(self):
+        """Initialize."""
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        """Get length."""
+        return self.size
+
+    def __iter__(self):
+        """Return an iterator."""
+        return self.root.__iter__()
+
+
 class TreeNode:
-  def __init__(self, key, val, left=None, right=None, parent=None):
-    self.key = key
-    self.val = val
-    self.left = left
-    self.right = right
-    self.parent = parent
-    
-  def has_left_child(self):
-    return self.left
-    
-  def has_right_child(self):
-    return self.right
-    
-  def is_left_child(self):
-    return self.parent and self.parent.left == self
-    
-  def is_right_child(self):
-    return self.parent and self.parent.right == self
-    
-  def is_root(self):
-    return not self.parent
-    
-  def has_any_children(self):
-    return self.left or self.right 
-    
-  def has_both_children(self):
-    return self.left and self.right
-    
-  def replace_node_data(self, key, value, left, right):
-    self.key = key
-    self.payload = value
-    self.left = left
-    self.right = right
-    if self.has_left_child():
-      self.left.parent = self
-    if self.has_right_child():
-      self.right.parent = self
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  
+
+    """Treenode class."""
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        """Initialize."""
+        self.key = key
+        self.val = val
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+    def has_left_child(self):
+        """Return true if there exists a left child."""
+        return self.left
+
+    def has_right_child(self):
+        """Return true if there exists a right child."""
+        return self.right
+
+    def is_left_child(self):
+        """Return true if node is a left child."""
+        return self.parent and self.parent.left == self
+
+    def is_right_child(self):
+        """Return true if node is a right child."""
+        return self.parent and self.parent.right == self
+
+    def is_root(self):
+        """Return true if node is the root node."""
+        return not self.parent
+
+    def has_any_children(self):
+        """Return true if node has children."""
+        return self.left or self.right
+
+    def has_both_children(self):
+        """Return true if node has both children."""
+        return self.left and self.right
