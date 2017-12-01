b537e56a56d83636802c8c3a8d466a6ce01a166b
Nora Petrova <schottkey@gmail.com>
2017-12-01 11:13:21 +0000

    Day 1 of Advent of Code

---
+++ advent-of-code/day1/inverse_captcha.py
@@ -0,0 +1,19 @@
+def read_input(filename: str) -> str:
+    with open(filename, 'r') as fr:
+        return fr.read().strip()
+
+
+def main() -> None:
+    seq = read_input('input')
+    seq += seq[0]
+    total = 0
+
+    for i in range(1, len(seq)):
+        if seq[i] == seq[i - 1]:
+            total += int(seq[i - 1])
+
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
