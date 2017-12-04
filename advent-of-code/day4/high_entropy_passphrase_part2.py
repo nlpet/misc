38e54dd636261e1cd86f9c6e73ed6447dc22f50e
Nora Petrova <schottkey@gmail.com>
2017-12-04 12:30:15 +0000

    Create high_entropy_passphrase_part2.py

---
+++ advent-of-code/day4/high_entropy_passphrase_part2.py
@@ -0,0 +1,17 @@
+
+def main():
+    num_valid = 0
+
+    with open('input.txt', 'r') as fr:
+        passwords = fr.readlines()
+
+    for password in passwords:
+        words = [''.join(sorted(w)) for w in password.strip().split(' ')]
+        if len(words) == len(set(words)):
+            num_valid += 1
+
+    print(num_valid)
+
+
+if __name__ == '__main__':
+    main()
