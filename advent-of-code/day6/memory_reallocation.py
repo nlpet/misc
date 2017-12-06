8ca125bb7990d2d5b3c102444fe7e2e1b360a55e
Nora Petrova <schottkey@gmail.com>
2017-12-06 13:12:02 +0000

    Update memory_reallocation.py

--- advent-of-code/day6/memory_reallocation.py
+++ advent-of-code/day6/memory_reallocation.py
@@ -1,45 +1,37 @@
 # --- Day 6: Memory Reallocation ---
-import numpy as np
 from datetime import datetime
+from typing import List, Set
+
+Memory = List[int]
+Cache = Set[str]
 
 
-def cycle(memory):
-    length = memory.size
-    index = np.argmax(memory)
+def cycle(memory: Memory) -> None:
+    length = len(memory)
+    index = memory.index(max(memory))
     max_blocks = memory[index]
     memory[index] = 0
-
-    if max_blocks >= length:
-        div = max_blocks // length
-        memory += div
-        max_blocks -= div * length
 
     while max_blocks > 0:
         index = (index + 1) % length
         memory[index] += 1
         max_blocks -= 1
 
-    # if max_blocks > 0:
-    #     if index + max_blocks >= l:
-    #         to_end = np.arange(index + 1, l)
-    #         from_start = np.arange(0, max_blocks - to_end.size)
-    #         indices = np.concatenate((from_start, to_end))
-    #     else:
-    #         indices = np.arange(index + 1, index + 1 + max_blocks)
-    #
-    #     memory[indices] += 1
+
+def to_string(array: Memory) -> str:
+    return ''.join([str(n) for n in array])
 
 
-def find_cycle(memory, seen):
-    while np.array_str(memory) not in seen:
-        seen.add(np.array_str(memory))
+def find_cycle(memory: Memory, seen: Cache) -> Cache:
+    while to_string(memory) not in seen:
+        seen.add(to_string(memory))
         cycle(memory)
     return seen
 
 
-def main():
+def main() -> None:
     with open('input.txt', 'r') as fr:
-        memory = np.array([int(n) for n in fr.read().strip().split('\t')])
+        memory = [int(n) for n in fr.read().strip().split('\t')]
 
     start = datetime.now()
 
