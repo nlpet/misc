05580174c226b302a7c373caa6a5265b95be05a3
Nora Petrova <schottkey@gmail.com>
2017-12-08 20:06:54 +0000

    Add solution to part 2 of Day 1

--- advent-of-code/2016/day1/taxicab.py
+++ advent-of-code/2016/day1/taxicab.py
@@ -9,27 +9,55 @@ from math import fabs
 from typing import Set, Tuple, List
 
 
+Coordinate = Tuple[int, int]
+CoordinatesSet = Set[Coordinate]
+CoordinatesList = List[Coordinate]
+
+
 def get_distance(x: int, y: int) -> int:
     return int(fabs(x) + fabs(y))
 
 
+def orient(facing: int, direction: str) -> int:
+    if direction[0] == 'R':
+        return (facing + 1) % 4
+    return (facing - 1) % 4
+
+
+def find_loc(coords: CoordinatesList, visited: CoordinatesSet) -> bool:
+    for coord in coords:
+        if coord in visited:
+            print(('\nFirst location visited twice is'
+                   ' {} blocks away'.format(get_distance(*coord))))
+            return True
+        visited.add(coord)
+    return False
+
+
 def move(directions: List[str]) -> None:
     facing, x, y = 0, 0, 0
+    found = False
+    visited: CoordinatesSet = set()
 
     for direction in directions:
         steps = int(direction[1:])
-        if direction[0] == 'R':
-            facing = (facing + 1) % 4
-        elif direction[0] == 'L':
-            facing = (facing - 1) % 4
+        facing = orient(facing, direction)
+
         if facing == 0:
+            coords = [(x, y) for y in range(y + 1, y + steps + 1)]
             y += steps
         elif facing == 1:
+            coords = [(x, y) for x in range(x + 1, x + steps + 1)]
             x += steps
         elif facing == 2:
+            coords = [(x, y) for y in range(y - steps, y)]
             y -= steps
         else:
+            coords = [(x, y) for x in range(x - steps, x)]
             x -= steps
+
+        if not found:
+            found = find_loc(coords, visited)
 
     dist = get_distance(x, y)
     num_steps = len(directions)
