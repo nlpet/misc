cf75bdec76a4dd7552e3260fbbb16cd59019f4b4
Nora Petrova <schottkey@gmail.com>
2017-12-12 15:11:34 +0000

    Add solution to part 2 of Day 12 (2017)

--- advent-of-code/2017/day12/digital_plumber.py
+++ advent-of-code/2017/day12/digital_plumber.py
@@ -6,32 +6,45 @@ from typing import List
 
 def main() -> None:
     group = defaultdict(set)
-    target = 0
 
-    with open('test.txt', 'r') as fr:
+    with open('input.txt', 'r') as fr:
         for line in fr.readlines():
             left, right = line.split('<->')
             program = int(left.strip())
             programs = [int(x) for x in right.strip().split(', ')]
             group[program] = group[program].union(set(programs))
 
-    stack: List[int] = list(group[target])
-    connected_to_target = set()
     visited = [0 for _ in range(len(group))]
+    all_programs = set(group.keys())
+    target_part1 = 0
+    connected_to_target_part1 = set([target_part1])
+    num_groups = 0
 
-    while stack:
-        connected = stack.pop()
-        if not visited[connected]:
-            connected_to_target.add(connected)
-            visited[connected] = 1
-
-        for connection in group[connected]:
-            if not visited[connection]:
-                stack.append(connection)
+    while all_programs:
+        target = all_programs.pop()
+        visited[target] = 1
+        stack: List[int] = list(group[target])
+        connected_to_target = set([target])
+        while stack:
+            connected = stack.pop()
+            if not visited[connected]:
+                connected_to_target.add(connected)
                 visited[connected] = 1
 
+            for connection in group[connected]:
+                if not visited[connection]:
+                    stack.append(connection)
+                    visited[connected] = 1
+
+            if target == target_part1:
+                connected_to_target_part1 = connected_to_target
+
+        all_programs = all_programs.difference(connected_to_target)
+        num_groups += 1
+
     print(('Number of programs in the group that contain '
-           'program ID 0 are {} '.format(len(connected_to_target))))
+           'program ID 0 are {} '.format(len(connected_to_target_part1))))
+    print('There are {} groups'.format(num_groups))
 
 
 if __name__ == '__main__':
