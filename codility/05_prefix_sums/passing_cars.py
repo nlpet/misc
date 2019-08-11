272c6194ec5f92913b10533af96e8a3a6e56bd3b
schottkey <schottkey@gmail.com>
2019-08-11 14:31:53 +0100

    Passing cars solution

--- codility/05_prefix_sums/passing_cars.py
+++ codility/05_prefix_sums/passing_cars.py
@@ -1,9 +1,18 @@
 def solution(A):
-    pass
+    n_passing, east_count, west_count = 0, 0, 0
+
+    for n in A:
+        if n == 0:
+            east_count += 1
+        else:
+            west_count += 1
+            n_passing += east_count
+
+    return -1 if n_passing > 1000000000 else n_passing
 
 
 def run_tests():
-    pass
+    assert solution([0, 1, 0, 1, 1]) == 5
 
 
 if __name__ == '__main__':
