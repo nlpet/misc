c812dfdf563b4a4fef866b13915b3da1421ab88d
Nora Petrova <schottkey@gmail.com>
2021-04-17 20:30:21 +0100

    Add Doge cipher #noregrets

--- doge_cipher.py
+++ doge_cipher.py
@@ -1,1 +1,54 @@
-# To Do
+import os
+import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+doge = """
+
+                  ▄              ▄
+                  ▌▒█           ▄▀▒▌
+                  ▌▒▒█        ▄▀▒▒▒▐
+                 ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+               ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+             ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+            ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌
+            ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+           ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌
+           ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌
+          ▌▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐
+          ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+          ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐
+           ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌
+           ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐
+            ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌
+              ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
+                ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀
+                   ▒▒▒▒▒▒▒▒▒▒▀▀
+
+"""
+
+def encrypt(msg, key):
+    key = key.zfill(32).encode('utf-8')
+    iv = os.urandom(16)
+    msg = msg.encode('utf-8')
+
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+    encryptor = cipher.encryptor()
+    ct = iv + encryptor.update(msg) + encryptor.finalize()
+    return base64.b64encode(ct).decode('utf-8')
+
+
+def decrypt(ct, key):
+    key = key.zfill(32).encode('utf-8')
+    ct = base64.b64decode(ct.encode('utf-8'))
+
+    iv = ct[:16]
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+
+    decryptor = cipher.decryptor()
+    msg = decryptor.update(ct[16:]) + decryptor.finalize()
+
+    try:
+        return msg.decode('utf-8')
+    except:
+        return msg
